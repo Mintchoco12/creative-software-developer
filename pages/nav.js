d0a1507db76d15171a6255e8ed2c5d2cb74b9368
Michael <michaelzhang5654@gmail.com>
2023-01-17 09:39:58 +0100

    Partial nav

---
+++ pages/nav.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from '../styles/Nav.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function nav() {
+    return (
+        <>
+            <div className={styles.navbar}>
+                <Link href="/">
+                <Image
+                    src="/glu_black.png"
+                    alt="GLU Logo"
+                    className={styles.gluLogo}
+                    width={200}
+                    height={90}
+                    priority
+                />
+                </Link>
+                <div className={styles.navbarRight}>
+                <Link href="/alumni" className={styles.navbarItem}>
+                    <p>Alumni</p>
+                </Link>
+                <Link href="https://www.glu.nl/de-vakschool-voor-creatieve-doeners/" className={styles.navbarItem}>
+                    <p>Over ons</p>
+                </Link>
+                <Link href="https://www.glu.nl/contact/" className={styles.navbarItem}>
+                    <p>Contact</p>
+                </Link>
+                </div>
+            </div>
+        </>
+  )
+}
